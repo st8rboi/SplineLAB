24bea95ab8966f635feca0acdf46f4faca1dc646
monkeyreel <radostevk80@yandex.ru>
2025-04-17 12:47:08 +0500

    add docstring for functions.py and nodes.py

--- code/functions.py
+++ code/functions.py
@@ -1,11 +1,17 @@
 import numpy as np
 
-def get_function(name):
-    if name == "sin(x)":
-        return np.sin
-    elif name == "1 / (1 + 25x^2)":
-        return lambda x: 1 / (1 + 25 * x**2)
-    elif name == "|x|":
-        return np.abs
-    elif name == "exp(x)":
-        return np.exp
+def get_function(name: str) -> callable:
+    """
+    Args:
+        name: имя (название) функции, которую требуется интерполировать.
+    Returns:
+        Возвращает ссылку на функцию, которая была выбрана пользователем.
+    Comment:
+        Допустимые варианты (будут обновляться): sin(x), 1 / (1 + 25x^2), |x|, exp(x) 
+    """
+    match name:
+        case "sin(x)": return np.sin
+        case "1 / (1 + 25x^2)": return lambda x: 1 / (1 + 25 * x**2)
+        case "|x|": return np.abs
+        case "exp(x)": return np.exp
+        case _: ...
--- code/nodes.py
+++ code/nodes.py
@@ -1,9 +1,23 @@
 import numpy as np
 
-def generate_nodes(a, b, n, kind):
-    if kind == "Равномерные":
-        return np.linspace(a, b, n)
-    elif kind == "Чебышёвские":
-        i = np.arange(n)
-        x = np.cos((2 * i + 1) / (2 * n) * np.pi)
-        return 0.5 * (a + b) + 0.5 * (b - a) * x
+def generate_nodes(a: float, b: float, n: int, kind: str) -> list[float]:
+    """
+    Args:
+        a: стартовая точка отрезка (включительно).
+        b: конечная точка отрезка (включительно).
+        n: количество узлов на отрезке.
+        kind: вид узлов (разбиения) сетки.
+    Returns:
+        Возвращает массив от a до b с n узлами.
+    Comment:
+        Допустимые варианты (будут обновляться): Равномерные, Чебышёвсике.
+    """
+    match kind:
+        case "Равномерные":
+            return np.linspace(a, b, n)
+        case "Чебышёвские":
+            i = np.arange(n)
+            x = np.cos((2 * i + 1) / (2 * n) * np.pi)
+            return 0.5 * (a + b) + 0.5 * (b - a) * x
+        case _:
+            ...
